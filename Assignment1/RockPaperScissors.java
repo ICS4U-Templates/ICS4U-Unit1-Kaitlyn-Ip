7c5bf5b39e9b1792110b40de218fe82098b52235
Kaitlyn Ip <kaitlyn.ip>
2024-03-21 13:00:59 +0000

    added java

--- Assignment1/RockPaperScissors.java
+++ Assignment1/RockPaperScissors.java
@@ -1,7 +1,7 @@
 /*
  * This program is the Rock-Paper-Scissors game
  *
- * By: Ava Venturino
+ * By: Kaitlyn Ip 
  * Version: 1.0
  * Since: 2024-03-07
  */
@@ -15,76 +15,76 @@ import java.util.Scanner;
 
 final class RockPaperScissors {
 
-/**
- * Rock constant, 1.
- */
-public static final string ROCK = "rock";
+    /**
+     * Rock constant, 1.
+     */
+    public static final String ROCK = "rock";
 
-/**
- * Paper constant, 1.
- */
-public static final string PAPER = "paper";
+    /**
+     * Paper constant, 1.
+     */
+    public static final String PAPER = "paper";
 
-/**
- * Scissors constant, 1.
- */
-public static final string SCISSORS = "scissors";
+    /**
+     * Scissors constant, 1.
+     */
+    public static final String SCISSORS = "scissors";
 
-/**
- * Prevent instantiation.
- * Throw an exception IllegalStateException.
- * if this is ever called
- *
- * @throws IllegalStateException if this is ever called
- *
- */
+    /**
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException.
+     * if this is ever called
+     *
+     * @throws IllegalStateException if this is ever called
+     *
+     */
 
-private RockPaperScissors() {
-    throw new IllegalStateException("Cannot be instantiated");
-}
-
-/**
- * The starting main() function.
- *
- * @param args No args will be used
- */
-
-public static void main(String[] args) {
-    // Generating random number between 0 and 2
-    final Random random = new Random();
-    final int randomNumber = random.nextInt(3);
-
-    // Input
-    final Scanner scanner = new Scanner(System.in);
-    System.out.print("Decide on a move (rock paper, or scissors): ");
-    final String player = scanner.nextLine().toLowerCase();
-
-    // Assigning computer's move based on the generated random number
-    final String[] moves = {ROCK, PAPER, SCISSORS};
-    final String computer = moves[randomNumber];
-
-    // Process & Output
-    if (player.equals(computer)) {
-        System.out.println("\nComputer chose " + computer);
-        System.out.println("\nTie.");
-    } else if (
-        ROCK.equals(player) && PAPER.equals(computer)
-            || PAPER.equals(player) && SCISSORS.equals(computer)
-            || SCISSORS.equals(player) && ROCK.equals(computer)
-    ) {
-        System.out.println("\nThe computer chose " + computer);
-        System.out.println("\nYou lost.");
-    } else if (
-        PAPER.equals(player) && ROCK.equals(computer)
-        || SCISSORS.equals(player) && PAPER.equals(computer)
-        || ROCK.equals(player) && SCISSORS.equals(computer)
-    ) {
-        System.out.println("\nThe Computer chose " + computer);
-        System.out.println("\nYou win!");
-    } else {
-        System.out.println("\nInvalid input.");
+    private RockPaperScissors() {
+        throw new IllegalStateException("Cannot be instantiated");
     }
 
-    System.out.println("\nDone.");
+    /**
+     * The starting main() function.
+     *
+     * @param args No args will be used
+     */
+
+    public static void main(String[] args) {
+        // Generating random number between 0 and 2
+        final Random randomNum = new Random();
+        final int randomNumber = randomNum.nextInt(3);
+
+        // Input
+        final Scanner scanner = new Scanner(System.in);
+        System.out.print("Decide on a move (rock paper, or scissors): ");
+        final String player = scanner.nextLine().toLowerCase();
+
+        // Assigning computer's move based on the generated random number
+        final String[] moves = {ROCK, PAPER, SCISSORS};
+        final String computer = moves[randomNumber];
+
+        // Process & Output
+        if (player.equals(computer)) {
+            System.out.println("\nComputer chose " + computer);
+            System.out.println("\nTie.");
+        } else if (
+            ROCK.equals(player) && PAPER.equals(computer)
+                || PAPER.equals(player) && SCISSORS.equals(computer)
+                || SCISSORS.equals(player) && ROCK.equals(computer)
+        ) {
+            System.out.println("\nThe computer chose " + computer);
+            System.out.println("\nYou lost.");
+        } else if (
+            PAPER.equals(player) && ROCK.equals(computer)
+                || SCISSORS.equals(player) && PAPER.equals(computer)
+                || ROCK.equals(player) && SCISSORS.equals(computer)
+        ) {
+            System.out.println("\nThe Computer chose " + computer);
+            System.out.println("\nYou win!");
+        } else {
+            System.out.println("\nInvalid input.");
+        }
+
+        System.out.println("\nDone.");
     }
 }
